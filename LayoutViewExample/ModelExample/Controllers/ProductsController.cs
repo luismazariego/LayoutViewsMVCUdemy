905666643b33f678f55fb05b51c2cb8be9788ee0
Luis José Mazariego <lmazariego@cgclatam.com>
2019-05-27 00:42:42 -0600

    ModelExample

---
+++ LayoutViewExample/ModelExample/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using ModelExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {ProductId = 101, ProductName = "AC", Rate = 45000},
+                new Product() {ProductId = 102, ProductName = "Mobile", Rate = 38000},
+                new Product() {ProductId = 103, ProductName = "Bike", Rate = 94000}
+            };
+
+            //ViewBag.products = products;
+
+            return View(products);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var products = new List<Product>()
+            {
+                new Product() {ProductId = 101, ProductName = "AC", Rate = 45000},
+                new Product() {ProductId = 102, ProductName = "Mobile", Rate = 38000},
+                new Product() {ProductId = 103, ProductName = "Bike", Rate = 94000}
+            };
+            Product matchingProduct = null;
+            foreach (var item in products)
+            {
+                if (item.ProductId == id)
+                    matchingProduct = item;
+            }
+
+            //ViewBag.MatchingProduct = matchingProduct;
+
+            return View(matchingProduct);
+        }
+    }
+}
