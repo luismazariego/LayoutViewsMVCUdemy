fdb7236d53b73afe3764519c7d227ce79c0184be
Luis José Mazariego <lmazariego@cgclatam.com>
2019-06-06 08:00:48 -0600

    single and multiple rows retrieving

--- EFDbFirstApproachExample/EFDbFirstApproachExample/Controllers/ProductsController.cs
+++ EFDbFirstApproachExample/EFDbFirstApproachExample/Controllers/ProductsController.cs
@@ -10,11 +10,35 @@ namespace EFDbFirstApproachExample.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string search = "")
         {
             var db = new EFDBFirstDatabaseEntities();
-            var products = db.Products.ToList();
+
+            //Conditional retrieve of items using Where method from linq.
+            //temp is a optional name related to the model, in this case the model is the same as the dataset
+            //dataset = model = Products.
+            //var products = db.Products.Where(temp => temp.CategoryID == 1 && temp.Price >= 50000).ToList();
+
+            //Here the condition is related to the search box in the view.
+            var products = db.Products.Where(temp => temp.ProductName.Contains(search)).ToList();
+
+            //to show the search term after it is submitted.
+            ViewBag.Search = search;
+
             return View(products);
+        }
+
+        public ActionResult Details(long id)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+
+            //this is the way showed by the instructor.
+            var product = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            
+            //More practical retrieve of data
+            //var product = db.Products.FirstOrDefault(temp => temp.ProductID == id);
+
+            return View(product);
         }
     }
 }
